126d68e74c3e9be52354c1b2182bd0d8de9650b9
Gavin <46956309+Gavin-Gao-Vegeta@users.noreply.github.com>
2024-05-12 20:29:55 +0800

    init

--- script/deploy_prd.sh
+++ script/deploy_prd.sh
@@ -14,4 +14,4 @@ zip -r $distName ./*
 serverPath="/data/wwwlemon8-web-ssr/dist-ssr"
 ssh bitnami@54.249.39.10 "mv $serverPath/$distName $serverPath/${distName}.bk"
 scp $distName bitnami@54.249.39.10:$serverPath
-ssh bitnami@54.249.39.10 "cd $serverPath;unzip -o $distName;cd server;lsof -iTCP:3000 | awk '{print $2}' | tail -n 1 | xargs kill;node nidex.mjs"
+ssh bitnami@54.249.39.10 "cd $serverPath;unzip -o $distName;cd server;lsof -iTCP:3000 | awk '{print $2}' | tail -n 1 | xargs kill;node index.mjs"
