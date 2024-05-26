72a67a5f5759926dfb8644f565af9b8ed9e9503a
Lenny-Zh <lenny.z@foxmail.com>
2024-05-26 13:45:21 +0800

    up deploy test

--- script/deploy_test.sh
+++ script/deploy_test.sh
@@ -18,4 +18,4 @@ serverPath="/data/wwwlemon8-web-ssr/dist-ssr"
 unzipPath="/data/wwwlemon8-web-ssr/dist"
 ssh $sshTarget "mkdir -p $serverPath;mkdir -p $unzipPath;rm -rf $unzipPath/*;mv $serverPath/$distName $serverPath/${distName}.bk"
 scp $distName $sshTarget:$serverPath
-ssh $sshTarget "cd $unzipPath;unzip -o $serverPath/$distName;cd server;lsof -iTCP:3000 | awk '{print $2}' | tail -n 1 | xargs kill;node index.mjs"
+ssh $sshTarget "cd $unzipPath;unzip -o $serverPath/$distName;cd server;lsof -iTCP:3000 | awk '{print $2}' | tail -n 1 | xargs kill;bash /data/wwwlemon8-web-ssr/monitor-lemon8-ssr.sh"
